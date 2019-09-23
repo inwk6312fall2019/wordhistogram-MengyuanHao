26c3f3578d11a081fae218532ce3c3f5ac5415ca
MengyuanHao <55468472+MengyuanHao@users.noreply.github.com>
2019-09-23 08:45:01 -0300

    Create part1.py

---
+++ part1.py
@@ -0,0 +1,13 @@
+def my_wordlist():
+    my_wordlist = open('words.txt')
+      for line in my_wordlist:
+            word = line.strip()
+            my_wordlist.append(word)
+      return my_wordlist
+def in_bisect(my_wordlist,word):
+   i = in_bisect(my_wordlist,word)
+     for m in my_newlist:
+        if m in i:
+          return index[m]
+        else:
+          return None
